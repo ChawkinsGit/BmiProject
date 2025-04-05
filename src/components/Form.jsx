c0614c619843feccefdbc95e9f4ccff53e39f6cf
ChawkinsGit <cmhawkins914@gmail.com>
2025-04-04 23:37:59 -0500

    fixed inputs to include only numbers

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const Form = () => {
-  const [bmi, setBmi] = useState({age: '', height: Number, weight: Number })
+  const [data, setData] = useState({age: '', height: '', weight: '' })
   const [inputVal, setInputVal] = useState()
   const calculateBmi = (height, weight) => {
     let bmi 
@@ -11,28 +11,54 @@ export const Form = () => {
     weight = (weight / 2.205)
 
     bmi = (weight / (height * height)) 
-    return bmi
+    console.log(bmi) 
   }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target; 
+    setData((prevData) => ({
+        ...prevData,
+        [name]: value === '' ? '' : Number(value) , // Update only the changed field
+    }));
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log("Submitted Data:", formData)
+
+}
   return (
     <>
-      <form className="row g-3">
+      <form className="row g-3" onSubmit={handleSubmit}>
         <div className="col-md-6">
           <label htmlFor="inputEmail4" className="form-label">Age</label>
-          <input type="text" className="form-control" id="inputEmail4" placeholder="Enter Age"/>
+          <input type="number" className="form-control" id="inputEmail4" placeholder="Enter Age"
+                name='age'
+                value={data.age} 
+                onChange={handleChange}  
+/>
         </div>
         <div className="col-md-6">
           <label htmlFor="inputPassword4" className="form-label">Height</label>
-          <input type="password" className="form-control" id="inputPassword4" placeholder="Enter Height(In Inches)"/>
+          <input type="number" className="form-control" id="inputPassword4" placeholder="Enter Height(In Inches)"
+                name='height'
+                value={data.height} 
+                onChange={handleChange}  
+          />
         </div>
         <div className="col-12">
           <label htmlFor="inputAddress" className="form-label">Weight</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="Enter Weight(lb)" />
+          <input type="number" className="form-control" id="inputAddress" placeholder="Enter Weight(lb)" 
+                name='weight'
+                value={data.weight} 
+                onChange={handleChange}  
+          />
         </div>
         
        
         
         <div className="col-12">
-          <button type="submit" className="btn btn-primary">Caclulate BMI</button>
+          <button onClick={calculateBmi} type="submit" className="btn btn-primary">Caclulate BMI</button>
         </div>
       </form>
     <Link to="/"><button>Back Home</button></Link>
